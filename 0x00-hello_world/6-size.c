1809ac963180412bba7d6f902fe452fe22f7500f
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-02-16 18:01:27 -0800

    the last oneee

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints the size of various types on 
+ * the computer it is compiled and run on.
  * Return: Always 0 (Success)
  */
 int main(void)
