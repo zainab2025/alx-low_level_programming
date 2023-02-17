076cc94ffb6a7baf0d526ca7dcb0559f35d96b1b
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-02-16 18:13:12 -0800

    c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types on 
- * the computer it is compiled and run on.
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
