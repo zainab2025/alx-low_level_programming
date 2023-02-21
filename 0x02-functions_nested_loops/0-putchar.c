5a573191909de05e5f2a32dcde113d41b78fdc03
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-02-21 14:56:54 -0800

    accepted

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#incude "main.h"
+#include "main.h"
 /**
  *main - Print _putchar
  *Return: Always 0
 */
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 7; i++)
-		_putchar (s[i]);
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 	return (0);
 }
+
