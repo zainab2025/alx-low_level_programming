7ea80b16a903f78a2453bbcffac9b7a9e66b26fd
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-03-06 18:58:23 -0800

    reg

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * _strpbrk - bytes
  * @s: pointer to char
  * @accept: pointer to char
  * Return: NULL
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
