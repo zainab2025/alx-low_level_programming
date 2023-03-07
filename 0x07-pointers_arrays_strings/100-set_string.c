7168c9fc887d0a5cac980d313afc9b24030f73d5
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-03-07 13:46:19 -0800

    last task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - resets @s to @to
+ * @s: the reference to a pointer that will be modified
+ * @to: the char reference to be set to @s
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
