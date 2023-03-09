76c3743f2e154725a303a9d67f9ee9c8e16467a3
root <zainb.1718150@stemismailia.moe.edu.eg>
2023-03-09 13:43:26 -0800

    lfk

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+#include <string.h>
+
 /**
- * _print_rev_recursion - Prints a string in reverse, followed by a new line.
- * @s: the string to be printed
- *
+ * _print_rev_recursion - print @s reversed to stdout, adds newline
+ * @s: the reference to a string
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(s[0]);
+		_putchar(*s);
 	}
 }
